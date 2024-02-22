e229a05d47666e8a42951e98d8d5ff5e06d5f813
black-lemonov <egor.porf1@mail.ru>
2024-02-22 11:13:51 +0300

    add html templates

--- fl_site.py
+++ fl_site.py
@@ -1,7 +1,17 @@
-from flask import Flask
+from flask import Flask, render_template
 
 # __name__ = file name
-app = Flask(__name__) 
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return render_template('index.html', title='О парсере')
+    
+
+@app.route('/about')  # обработчик для url: '/about'
+def about():
+    return '<h3>О нас:</h3><p>здесь будет сайт с подборками новостей</p>'
+
 
 if __name__ == "__main__":
     app.run(debug=True)
